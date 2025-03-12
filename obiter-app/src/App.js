47d8cf64e9989c10a5f4959442cb45bed05ff1e3
Itamar Ben Nun <itamarbennun@mail.tau.ac.il>
2025-03-12 21:33:13 +0200

    some home page, folders created

--- obiter-app/src/App.js
+++ obiter-app/src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import calender from './assets/images/calender.svg'
+import calculator from './assets/images/calculator.svg'
+import chat from './assets/images/chat.svg'
+import check from './assets/images/check.svg'
+import flashcards from './assets/images/flashcards.svg'
+import progress from './assets/images/progress.svg'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Obiter</h1>
+        <div className="pageButtons">
+          <img src={chat}/>
+          <img src={flashcards}/>
+          <img src={calender}/>
+          <img src={calculator}/>
+          <img src={progress}/>
+          <img src={check}/>
+        </div>
       </header>
     </div>
   );
